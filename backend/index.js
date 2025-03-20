c09368c7081211a954fa157acdaa9eaefd06f9d0
Rahul12KumarSingh <2021ugec091@nitjsr.ac.in>
2025-03-20 17:47:20 +0530

    media support added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,37 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 
+const {Storage} = require('@google-cloud/storage');
+
+const storage  = new Storage({
+  keyFilename: "./config/keys.json",
+});
+
+const bucketName = "chatappstorage"
+
+
+async function generateSignedUrl(fileName , fileType){
+       const bucket = storage.bucket(bucketName);
+
+       const file = bucket.file(fileName);
+
+       const options = {
+          version : 'v4',
+          action : 'write',
+          expires : Date.now() + 15 * 60 * 1000, 
+          contentType : fileType,
+       }
+
+       const [url] = await file.getSignedUrl(options);
+      
+       console.log("generated signed url : " , url) ;
+       return url ;
+}
+
+
+
+
+
 const  ChatUser  = require("./models/chatuser");
 const  User  = require("./models/user");
 
@@ -73,6 +104,27 @@ app.get('/' , (req , res)=>{
      res.json({
             message : 'Server is running up!!!' ,
         });
+}) ;
+
+
+
+//end points to generate the signed urls.....
+app.get('/generateSignedUrl' , async(req , res)=>{
+  try {
+     console.log("hiii");
+
+     const {fileName , fileType} = req.query ;
+
+     if(!fileName || !fileType){
+        return res.status(400).json({error : 'Missing required parameters'});
+     }
+
+     const signedUrl = await generateSignedUrl(fileName , fileType);
+
+     res.json({signedUrl});
+  } catch (error) {
+     res.status(500).json({error : error.message});
+  }
 }) ;
 
 
--- client/src/SingleChats.js
+++ client/src/SingleChats.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState ,  useRef  } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import "./Chats.css";
 import io from "socket.io-client";
@@ -6,12 +6,45 @@ import io from "socket.io-client";
 const ENDPOINT = "http://localhost:5000";
 let socket;
 
+
+
 export default function SingleChats({ user, chats, setChats, selectedChat }) {
+
+
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [notification, setNotification] = useState([]);
 
-  const processedMessage = useRef() ;
+  const [downloadProgress, setDownloadProgress] = useState({});
+
+  const processedMessage = useRef();
+
+
+  const [file, setFile] = useState(null);
+
+  //logics for the upload files....
+  const handleFileChange = async (e) => {
+    setFile(e.target.files[0]);
+  }
+
+  const uploadFile = async () => {
+    const response = await axios.get(`http://localhost:5000/generateSignedUrl?fileName=${file.name}&fileType=${file.type}`);
+
+    console.log("Response : ", response);
+
+    const { signedUrl } = response.data;
+
+    console.log("signed url : ", signedUrl);
+
+    await axios.put(signedUrl, file, {
+      headers: {
+        'Content-Type': file.type
+      },
+    });
+
+    console.log("File uploaded successfully", signedUrl.split("?")[0]);
+
+  }
 
   // Fetch messages when a chat is selected
   const fetchMessage = async () => {
@@ -22,24 +55,23 @@ export default function SingleChats({ user, chats, setChats, selectedChat }) {
 
     const { data } = await axios.get(`http://localhost:5000/messages/${selectedChat}?userId=${user.id}`);
 
-    console.log("Messages  : ", data); 
+    console.log("Messages  : ", data);
 
     //update the unseen message count of that chat to 0...
     setChats((prevChats) =>
-           prevChats.map((chat) => {
-            if (chat.id === selectedChat) {
-              return { ...chat, unseenMessagesCount: 0};
-            }
-            return chat ;
+      prevChats.map((chat) => {
+        if (chat.id === selectedChat) {
+          return { ...chat, unseenMessagesCount: 0 };
+        }
+        return chat;
       })
     );
 
     setMessages(data);
   };
 
-  const makeUserInactiveForSelectedChat = async()=>{
-    if(selectedChat)
-    {
+  const makeUserInactiveForSelectedChat = async () => {
+    if (selectedChat) {
       console.log("makeUserInactiveForSelectedChat : ", selectedChat);
       await axios.put(`http://localhost:5000/chats/${selectedChat}?userId=${user.id}`);
     }
@@ -49,39 +81,39 @@ export default function SingleChats({ user, chats, setChats, selectedChat }) {
     fetchMessage();
 
     return () => {
-      makeUserInactiveForSelectedChat(selectedChat); 
+      makeUserInactiveForSelectedChat(selectedChat);
     };
 
   }, [selectedChat]);
 
   //Send Message....
   const sendMessage = async () => {
-    console.log("user info : " , user) ;
+    console.log("user info : ", user);
 
     if (!newMessage) return;
 
     const { data } = await axios.post("http://localhost:5000/messages/send", {
       chatId: selectedChat,
       senderId: user.id,
-      senderName:user.name,
+      senderName: user.name,
       content: newMessage,
     });
 
     socket.emit("sendMessage", data);
     setMessages([...messages, data]);
-    
+
 
     //update the latest message in the chat
     setChats((prevChats) =>
       prevChats.map((chat) => {
-        if (chat.id ===  selectedChat) {
-          return { ...chat , latestMessage : newMessage};
+        if (chat.id === selectedChat) {
+          return { ...chat, latestMessage: newMessage };
         }
-        return chat ;
+        return chat;
       })
-     );
+    );
 
-     setNewMessage("");
+    setNewMessage("");
 
   };
 
@@ -96,37 +128,35 @@ export default function SingleChats({ user, chats, setChats, selectedChat }) {
     socket.emit("setup", user.id);
   }, []);
 
-  // Handle incoming messages
+  //Handle incoming messages......
   useEffect(() => {
     const handleReceiveMessage = (msg) => {
-      
-      if(processedMessage.current === msg.id)return ;
 
-      processedMessage.current = msg.id ;
-      
+      if (processedMessage.current === msg.id) return;
+
+      processedMessage.current = msg.id;
+
       console.log("Message received from server : ", msg);
 
-      if (msg.chatId !== selectedChat)
-       {
+      if (msg.chatId !== selectedChat) {
         setChats((prevChats) =>
           prevChats.map((chat) => {
             if (chat.id === msg.chatId) {
-              return { ...chat, unseenMessagesCount: chat.unseenMessagesCount + 1 , latestMessage : msg.content};
+              return { ...chat, unseenMessagesCount: chat.unseenMessagesCount + 1, latestMessage: msg.content };
             }
-            return chat ;
+            return chat;
           })
-         );
-       }
-     else
-      {
+        );
+      }
+      else {
         setChats((prevChats) =>
           prevChats.map((chat) => {
             if (chat.id === msg.chatId) {
-              return { ...chat, latestMessage : msg.content};
+              return { ...chat, latestMessage: msg.content };
             }
-            return chat ;
+            return chat;
           })
-         );
+        );
 
         setMessages((prev) => [...prev, msg]);
       }
@@ -135,6 +165,37 @@ export default function SingleChats({ user, chats, setChats, selectedChat }) {
     socket.on("receiveMessage", handleReceiveMessage);
     return () => socket.off("receiveMessage", handleReceiveMessage);
   }, [selectedChat]);
+
+  //....//
+  const downloadFile = async (fileUrl, fileName) => {
+    try {
+      const response = await axios.get(fileUrl, {
+        responseType: "blob",
+        onDownloadProgress: (progressEvent) => {
+          const percent = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          console.log("Percent : ", percent);
+          setDownloadProgress((prev) => ({ ...prev, [fileName]: percent }));
+        }
+      })
+
+      const blob = new Blob([response.data]);
+      const fileURL = URL.createObjectURL(blob);
+
+
+      const link = document.createElement("a");
+      link.href = fileURL;
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      //opening the file when it is downloaded...
+      // window.open(fileUrl);
+    }
+    catch (err) {
+      console.log("error happen in downloading the file : ", err.message);
+    }
+  }
 
   return (
     <div className="messages-section">
@@ -148,7 +209,48 @@ export default function SingleChats({ user, chats, setChats, selectedChat }) {
                 {m.senderName} : {m.content}</div>
             ))}
           </div>
+
+          <div style={{ position: "relative", display: "inline-block" }}>
+            {downloadProgress["Rahul Resume"] !== undefined && (
+              <div
+                style={{
+                  position: "absolute",
+                  top: "0",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  backgroundColor: "rgba(0, 0, 0, 0.7)",
+                  color: "white",
+                  padding: "5px",
+                  borderRadius: "5px",
+                  fontSize: "12px",
+                }}
+              >
+                {downloadProgress["Rahul Resume"]}% Downloaded
+              </div>
+            )}
+
+            <embed
+              src="https://storage.googleapis.com/chatappstorage/rahulfinal.pdf"
+              type="application/pdf"
+              width="150px"
+              height="150px"
+              style={{ cursor: "pointer", border: "1px solid black" }}
+              onClick={() => downloadFile("https://storage.googleapis.com/chatappstorage/rahulfinal.pdf", "Rahul Resume")}
+            />
+          </div>
+
+
           <div className="message-input">
+
+            <input
+              type="file"
+              onChange={handleFileChange}
+            />
+
+            <button onClick={uploadFile}>Upload</button>
+
+
+
             <input
               type="text"
               value={newMessage}
